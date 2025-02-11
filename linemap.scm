3ae128705ef9050c7434bc6e53ba31fc1956b550
Saul James Tobin <saul.james.tobin@gmail.com>
2025-02-11 16:04:27 -0500

    Update license headers

--- linemap.scm
+++ linemap.scm
@@ -1,3 +1,22 @@
+;;; linemap.scm --- Moment-location mapping for LilyPond
+
+;; Copyright (c) 2025 Saul James Tobin
+
+;; This file is part of lilyond-ts-mode.
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with lilypond-ts-mode.  If not, see <https://www.gnu.org/licenses/>.
+
 (use-modules (ice-9 and-let-star)
              (srfi srfi-26)
              (srfi srfi-71))
