4b7d1c020f661ab36460427c9ec80edb9832eeee
Saul James Tobin <saul.james.tobin@gmail.com>
2025-03-04 04:38:53 -0500

    Fix bugs and refactor nav rewrite

--- navigation-3.scm
+++ navigation-3.scm
@@ -18,12 +18,14 @@
 ;; along with lilypond-ts-mode.  If not, see <https://www.gnu.org/licenses/>.
 
 (use-modules (ice-9 and-let-star)
-             (srfi srfi-26)
-             (srfi srfi-71))
+             (srfi srfi-26))
 
 ((@@ (lily) translator-property-description)
  'navigationTable list? "Collect a list of the first input location for every
 moment in each Bottom context.")
+((@@ (lily) translator-property-description)
+ 'navigationExportProps symbol-list? "Context properties to include in exported
+navigation data.")
 
 (define-public (selective-map-contexts proc ctx)
   "Apply @var{proc} to context @var{ctx}.  If @var{proc} returns @code{#f},
@@ -41,23 +43,27 @@ recurse over the children of @var{ctx}. Returns the tree of contexts where
   (eq? ctx (ly:context-property-where-defined ctx 'currentBarNumber)))
 
 (define ((record-origins-listener ctx) event)
-  (and-let* (((location (ly:event-property event 'origin #f)))
-             (fl-ln-ch-cl (ly:input-file-line-char-column location))
+  (let ((location (ly:event-property event 'origin #f)))
+    (when location
+      (let* ((fl-ln-ch-cl (ly:input-file-line-char-column location))
              (now (ly:context-current-moment ctx))
-             (barn (ly:context-property ctx 'currentBarNumber))
-             (mpos (ly:context-property ctx 'measurePosition))
+             (export-props (ly:context-property ctx 'navigationExportProps
+                                                '(measurePosition
+                                                  currentBarNumber)))
+             (extra-data (filter-map (cut ly:context-property ctx <> #f)
+                                     export-props))
              (ev-length (ly:event-property event 'length ZERO-MOMENT))
              (end (if (= 0 (ly:moment-grace now))
                       (+ now ev-length)
                       (- now (ly:make-moment 0 (- (ly:moment-main ev-length))))))
              (score-ctx (ly:context-find ctx 'Score))
              (nav-table (ly:context-property score-ctx 'navigationTable)))
-    (ly:context-set-property! score-ctx 'navigationTable
-                              (assoc-set! nav-table (car fl-ln-ch-cl)
-                                          (cons (list fl-ln-ch-cl
-                                                      now end barn mpos)
-                                                (assoc-get (car fl-ln-ch-cl)
-                                                           nav-table '()))))))
+        (ly:context-set-property! score-ctx 'navigationTable
+                                  (assoc-set! nav-table (car fl-ln-ch-cl)
+                                              (cons (cons* fl-ln-ch-cl
+                                                           now end extra-data)
+                                                    (assoc-get (car fl-ln-ch-cl)
+                                                               nav-table '()))))))))
 
 (define (record-origins! ctx)
   (and (has-timing? ctx)
@@ -92,78 +98,75 @@ result in reverse order (slightly more efficient)."
            (test (s a) (s b)))))
 
 (define (sort-by-location file-table)
-  (uniq-list (sort-list file-table (compare-places > cadar caddar))
-             #:key cdar #:reverse? #t))
+  (cons (car file-table)
+        (uniq-list (sort-list (cdr file-table) (compare-places > cadar caddar))
+                   #:key cdar #:reverse? #t)))
 
 (define* (split-sequential-segments lst #:key (key identity) (test <))
   (fold (lambda (x acc)
-          ;; This awkward let block is because Guile lacks prog1/begin0
-          (let ((res (if (and (pair? acc)
-                              (test (key (cdaar acc))
-                                    (key x)))
-                         (cons (cons x (car acc))
-                               (cdr acc))
-                         (cons (list x) acc))))
-            ;; The purpose of doing this is to smuggle the segment indices into
-            ;; the file-collated list without having to iterate over the entire
-            ;; list to reverse-lookup every location
-            (set-cdr! x (cons (1- (length res)) (cdr x)))
-            res))
+          (if (and (pair? acc)
+                   (test (key (caar acc))
+                         (key x)))
+              (cons (cons x (car acc))
+                    (cdr acc))
+              (cons (list x) acc)))
         '() lst))
 
-(define-public book-nav-tables (make-parameter (list)))
+(define (distribute-index! i seg)
+  (map (lambda (elt)
+         (set-cdr! elt (cons* i (map (match-lambda ((? ly:moment? m)
+                                                    (moment->nav-key m))
+                                                   (x x))
+                                     (cdr elt))))
+         (match elt ((loc j mom end . data)
+                     `((,mom . ,end) ,loc ,data))))
+       seg))
+
+(define (moment->nav-key m)
+  (exact->inexact (ly:moment-main m)))
+
+(define (merge-collated-nav-tables alist1 alist2)
+  (if (null? alist1)
+      alist2
+      (fold (lambda (ftable accum)
+              (assoc-set! accum (car ftable)
+                          (merge (assoc-get (car ftable) accum '())
+                                 (cdr ftable)
+                                 (compare-places < caar cadar))))
+            alist1 alist2)))
+
+(define-public book-score-nav-tables (make-parameter (list)))
+(define-public book-collated-nav-tables (make-parameter (list)))
 
 (define-public (Record_locations_translator ctx)
   (make-translator
    ((initialize translator)
-    (selective-map-contexts has-timing? record-origins! ctx))
+    (selective-map-contexts record-origins! ctx))
+
    ((finalize translator)
+    (ly:progress "\nCollating navigation data...")
     (let* ((nav-table (ly:context-property ctx 'navigationTable))
            (by-file-sorted (map sort-by-location nav-table))
            (seq-segs (append-map (compose reverse
                                           (cut split-sequential-segments <>
                                                #:key cadr)
                                           cdr)
+                                 by-file-sorted))
+           (by-moment (index-map distribute-index! seq-segs))
+           (score-id (string->symbol (format #f "~a-~d-~d"
+                                             current-outfile-name
+                                             (hash input-file-name 999)
+                                             (length (book-score-nav-tables)))))
+           (by-file-indexed (map (lambda (ftable)
+                                   (cons (car ftable)
+                                         (map (match-lambda
+                                                (((fname . floc) . data)
+                                                 (cons* floc score-id data)))
+                                              (cdr ftable))))
                                  by-file-sorted)))
-      (book-nav-tables `(((by-file . ,by-file-sorted)
-                          (by-moment . ,seq-segs))
-                         ,(book-nav-tables)))))))
-
-(define (moment->nav-key m)
-  (exact->inexact (ly:moment-main m)))
-
-(define (merge-nav-file-alists by-file-alists)
-  (reduce (lambda (alist1 alist2)
-            (fold (lambda (ftable accum)
-                    (assoc-set! accum (car ftable)
-                                (merge (assoc-get (car ftable) accum '())
-                                       (cdr ftable)
-                                       (compare-places < caar cadar))))
-                  alist1 alist2))
-          '() by-file-alists))
-
-(define (make-score-id index)
-  (string->symbol
-   (format #f "~a-~d-~d" current-outfile-name (hash input-file-name 999) index)))
-
-(define-public (map-squared proc lst)
-  (map (cut map proc <>) lst))
-
-(define (distribute-indices score-index score-table)
-  (let* ((score-id (make-score-id score-index))
-         (by-file (map-squared (match-lambda
-                                 (((fname . floc) j . rhythmic-data)
-                                  (list-set! rhythmic-data 0
-                                             (moment->nav-key (car rhythmic-data)))
-                                  (list-set! rhythmic-data 1
-                                             (moment->nav-key (cadr rhythmic-data)))
-                                  `(,floc (,score-id ,j) ,rhythmic-data)))
-                               (assq-ref score-table 'by-file)))
-         (by-moment (map-squared (match-lambda
-                                   ((loc j mom end . other-rhythmic-data)
-                                    `((,mom ,end) ,loc ,other-rhythmic-data)))
-                                 (assq-ref score-table 'by-moment))))
-    (cons by-file (cons score-id by-moment))))
+      (book-score-nav-tables (acons score-id by-moment (book-score-nav-tables)))
+      (book-collated-nav-tables (merge-collated-nav-tables (book-collated-nav-tables)
+                                                           by-file-indexed))))))
 
 (define-public (write-file-to-subdir data name dir ext)
   (let* ((parent-dir (dirname (or input-file-name name)))
@@ -175,17 +178,10 @@ result in reverse order (slightly more efficient)."
     (ly:progress "\nConverting to `~a'..." fname)
     (close-port-rename tmp fname)))
 
-(define-public (make-book-nav-table)
-  (let* ((format-elts (index-map distribute-indices (book-nav-tables)))
-         (by-score (map cdr format-elts))
-         (by-file (merge-nav-file-alists (map car format-elts))))
-    (ly:progress "\nExporting navigation data for book `~a'..."
-                 current-outfile-name)
-    (write-file-to-subdir `((by-score . ,by-score)
-                            (by-input-file . ,by-file))
-                          book-name ".nav" ".l")))
-
 (define (default-toplevel-book-handler book)
-  (parameterize ((book-nav-tables (list)))
+  (parameterize ((book-score-nav-tables (list))
+                 (book-collated-nav-tables (list)))
     (toplevel-book-handler book)
-    (make-book-nav-table)))
+    (write-file-to-subdir `((by-score . (book-score-nav-tables))
+                            (by-input-file . (book-collated-nav-tables)))
+                          current-outfile-name ".nav" ".l")))
